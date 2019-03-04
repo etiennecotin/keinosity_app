55093e344a6ba18513c1a7973671464c01906aca
Etienne Cotin <etienne.cotin@gmail.com>
2019-03-05 00:46:39 +0100

    add logout

--- src/screens/ProfilScreen.js
+++ src/screens/ProfilScreen.js
@@ -1,19 +1,31 @@
 
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
+
 
 export default class ProfilScreen extends React.Component {
     static navigationOptions = {
         title: 'Profil',
     };
+
+    _logout = async () => {
+
+        const userToken = await AsyncStorage.getItem('userToken');
+        if (userToken) {
+            AsyncStorage.removeItem('userToken');
+            AsyncStorage.removeItem('refreshToken');
+            this.props.navigation.navigate('AuthLoading')
+        }
+    };
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>profil Screen</Text>
-                {/*<Button*/}
-                {/*title="Add some friends"*/}
-                {/*onPress={() => this.props.navigation.navigate('Details')}*/}
-                {/*/>*/}
+                <Button
+                    title="Déconnexion"
+                    onPress={this._logout}
+                />
             </View>
         );
     }
