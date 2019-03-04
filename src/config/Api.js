3ca4685393c2a670b70d13418692063945c2fc3d
Etienne Cotin <etienne.cotin@gmail.com>
2019-03-05 00:45:50 +0100

    Update config api

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,6 +1,22 @@
 import axios from "axios";
+import {AsyncStorage} from "react-native";
 
-export default axios.create({
+const getToken = async () => {
+    try {
+        const userToken = await AsyncStorage.getItem('userToken').then(function(data) {
+            // loaded
+            return data;
+        });
+        if (userToken !== null) {
+            return userToken;
+        }
+        return '';
+    } catch (error) {
+        // Error retrieving data
+    }
+};
+
+const axiosInstance = axios.create({
     baseURL: 'http://keinosity.etienne-cotin.fr/api/',
     // timeout: 1000,
     headers:{
@@ -9,3 +25,14 @@ export default axios.create({
         // 'Accept': 'application/json'
     }
 });
+
+axiosInstance.interceptors.request.use(
+async config => {
+            const token = await getToken();
+            config.headers.Authorization = `Bearer ${token}`;
+            return config;
+    },
+    error => Promise.reject(error)
+);
+
+export default axiosInstance;
