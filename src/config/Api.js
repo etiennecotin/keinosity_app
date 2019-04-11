cf8ea12685011143b8c96486be8c4829078a6590
Etienne Cotin <etienne.cotin@gmail.com>
2019-04-11 19:33:04 +0200

    configure header

--- src/config/Api.js
+++ src/config/Api.js
@@ -4,7 +4,6 @@ import {AsyncStorage} from "react-native";
 const getToken = async () => {
     try {
         const userToken = await AsyncStorage.getItem('userToken').then(function(data) {
-            // loaded
             return data;
         });
         if (userToken !== null) {
@@ -20,9 +19,8 @@ const axiosInstance = axios.create({
     baseURL: 'http://keinosity.etienne-cotin.fr/api/',
     // timeout: 1000,
     headers:{
-        // 'Content-Type': 'application/x-www-form-urlencoded',
         'Content-Type': 'application/json',
-        // 'Accept': 'application/json'
+        'Accept': 'application/json'
     }
 });
 
