b4ac3e332f6979e0bfdc72c885c110a05bc1a226
Etienne Cotin <etienne.cotin@gmail.com>
2019-04-11 19:40:13 +0200

    add login screen

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
-import {StyleSheet, Text, View, Button, StatusBar, TextInput} from 'react-native';
+import {StyleSheet, Text, View, Button, StatusBar, TextInput, ImageBackground, Image} from 'react-native';
 import Api from "../config/Api";
 import LoginForm from '../form/login_form'
+import splashScreen from '../assets/splashscreen.png'
+import logo_mini from '../assets/logo_mini_white.png'
 
 export default class LoginScreen extends React.Component {
     static navigationOptions = {
@@ -12,11 +14,14 @@ export default class LoginScreen extends React.Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Login Screen</Text>
-                <LoginForm
-                    navigate={navigate}/>
-            </View>
+            <ImageBackground source={splashScreen} style={{width: '100%', height: '100%'}}>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Image source={logo_mini} style={{width: 60, height: 60}} resizeMode={`contain`}/>
+                    <Text style={{marginTop: 15, marginBottom: 15, color: '#fff', fontSize: 18}}>Connexion</Text>
+                    <LoginForm
+                        navigate={navigate}/>
+                </View>
+            </ImageBackground>
         );
     }
 }
